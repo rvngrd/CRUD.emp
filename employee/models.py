9860b4501a711cce43247cb5d1505ad70d3f9790
rvngrd <rrvngrdd@gmail.com>
2022-12-31 20:40:26 +0330

    Employee model created in models.py

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    eid = models.CharField('آیدی', max_length=20)
+    ename = models.CharField('نام', max_length=100)
+    elname = models.CharField('نام خانوادگی', max_length=20)
+    eemail = models.EmailField('ایمیل')
+    econtact = models.CharField('شماره تماس', max_length=11)
+
+    class Meta:
+        db_table = 'employee'
+        verbose_name = 'کارمند'
+        verbose_name_plural = 'کارمندان'
